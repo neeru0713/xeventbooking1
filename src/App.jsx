ced84f730914f7999e3ea0176af6a53d5a3fc55c
neeru0713 <neerurani1307@gmail.com>
2026-03-31 15:45:09 +0530

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import React,{ useEffect, useState } from "react";
 import axios from "axios";
 
 // ---------------- HOME PAGE ----------------
